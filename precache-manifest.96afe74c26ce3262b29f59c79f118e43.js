59b4cf5d2f8f29864aa8579a3fceb112c82fd0bd
sandy ak <sandeshak4777@gmail.com>
2020-12-14 08:54:04 +0530

    Updates

---
+++ precache-manifest.96afe74c26ce3262b29f59c79f118e43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7014e9a5d56ec53939ebafa978d7ea23",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe9fd7028bbaf5ffe3f0",
+    "url": "/static/css/2.69507427.chunk.css"
+  },
+  {
+    "revision": "6a1b2377ddcba1727c01",
+    "url": "/static/css/main.cbb6ce5b.chunk.css"
+  },
+  {
+    "revision": "fe9fd7028bbaf5ffe3f0",
+    "url": "/static/js/2.774c6775.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/static/js/2.774c6775.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a1b2377ddcba1727c01",
+    "url": "/static/js/main.0b6c921f.chunk.js"
+  },
+  {
+    "revision": "aeea36bdadbfd99c7e94",
+    "url": "/static/js/runtime-main.f7969a42.js"
+  }
+]);
